aa6806975f7586a08335b5d5b5e5cbb11a9621eb
nsitum <nenositum@gmail.com>
2025-03-14 11:24:09 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5d0191c9-1701-4c2a-bb28-0b7903c30660';
+export const MODAL_CLOSE_SEC = 2.5;
